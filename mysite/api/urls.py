24c9687c5990d7ee30a9430463990379de5c961a
guregu <takosuper1@gmail.com>
2024-09-20 16:09:14 -0400

    modified data in data base. Finished TWT django api course.

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("blogposts/", views.BlogPostListCreate.as_view(), name="blogpost-view-create"),
+    path("blogposts/<int:pk>/", views.BlogPostRetrieveUpdateDestroy.as_view(), name="update")
 ]
